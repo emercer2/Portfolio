ce47f770c6142f36c7cf33c3e4bdd390c04e1801
emercer2UNB3035 <77587896+emercer2UNB3035@users.noreply.github.com>
2024-06-09 15:13:13 -0300

    Starting to fill up front page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import headshot from './media/headshot.png';
+import headshotOutline1 from './media/headshotOutline1.png';
+import headshotOutline2 from './media/headshotOutline2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Elizabeth Mercer's Portfolio</h1>
+        <img src={headshot} className="headshot" alt="logo" />
+        <img src={headshotOutline1} className="headshot-outline-1" alt="logo" />
+        <img src={headshotOutline2} className="headshot-outline-2" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
