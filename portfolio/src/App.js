bf90c8947551a54c046b78f938a0e08a50c23407
emercer2UNB3035 <77587896+emercer2UNB3035@users.noreply.github.com>
2024-06-14 11:09:34 -0300

    Some UI updates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,13 +8,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Elizabeth Mercer's Portfolio</h1>
-        <img src={headshot} className="headshot" alt="logo" />
-        <img src={headshotOutline1} className="headshot-outline-1" alt="logo" />
-        <img src={headshotOutline2} className="headshot-outline-2" alt="logo" />
-        <p>
-          Welcome to my first portfolio ever!
-          Also my first time using react. That is some pretty exciting stuff right there. Soon I will have different projects here to show off the cool things I am capable of.
-        </p>
+        <div className='row'>
+          <img src={headshot} className="headshot" alt="logo" />
+          <img src={headshotOutline1} className="headshot-outline-1" alt="logo" />
+          <img src={headshotOutline2} className="headshot-outline-2" alt="logo" />
+          <p className='intro-text'>
+            Welcome to my first portfolio ever!
+            Also my first time using react. That is some pretty exciting stuff right there. Soon I will have different projects here to show off the cool things I am capable of.
+          </p>
+        </div>
+        <div>
+          <button className='basic-button'>Contact Me</button>
+          <button disabled className='basic-button'>Portfolio Prijects</button>
+
+        </div>
       </header>
     </div>
   );
